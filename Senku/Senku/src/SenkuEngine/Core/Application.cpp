da19e73cff8a375c9e869191d8f22706dc5fe884
ArsenM <a.masiuk@elsman.eu>
2022-07-19 22:56:13 +0300

    initial commit

---
+++ Senku/Senku/src/SenkuEngine/Core/Application.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "Application.h"
+
+
+Senku::Application::Application()
+{
+}
+
+Senku::Application::~Application()
+{
+}
+
+void Senku::Application::Run()
+{
+	while (m_Running)
+	{
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char** argv)
+{
+	// todo: init logger 
+
+	// todo: create app
+
+	// run app
+
+	system("pause");
+	return 0;
+
+}
---
+++ Senku/Senku/src/SenkuEngine/Core/Application.h
@@ -0,0 +1,32 @@
+#pragma once
+
+
+
+
+namespace Senku
+{
+	class Application
+	{
+	public:
+		Application();
+		~Application();
+
+
+
+
+	private:
+		// events
+
+	private:
+
+		void Run();
+
+
+
+	private:
+		//members
+		bool m_Running = true;
+
+	};
+
+}
